0a142c84e1dd223c7ecda3b2d3b1e20dce1796a6
phayo <mrphayo@MrPhayo-PC.localdomain>
2019-11-21 16:31:08 +0100

    Create component Transaction list item

--- src/views/Homepage/Home.js
+++ src/views/Homepage/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Transaction from '../../components/transaction/Transaction';
 
 
 export default function Home() {
     return (
         <div>
-
             This is the homepage
+            <Transaction/>
         </div>
     )
 }
